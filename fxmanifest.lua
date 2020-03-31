7a5fe444c0ad06e9bca2f326332efa3dc1048b5d
ItsPowXX <61159825+ItsPowXX@users.noreply.github.com>
2020-03-31 11:14:26 +0300

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,17 @@
+fx_version 'adamant'
+games {'gta5'}
+
+description 'Location de vehicule By PowX'
+
+client_script {
+    'cl_framework.lua',
+    'cl_location.lua'
+}
+
+server_script { 
+    'sv_location.lua'
+}
+
+
+
+--By PowX#2629
